32de7bd18afa27d92a82ccfa56e21f7e262e9df2
Sorin Vatasoiu <sorin.vatasoiu@gmail.com>
2014-09-30 00:45:12 -0400

    made item uploading more robust and added timestamp, time left, and number of bids left fields

--- Ebay Analyzer/src/item.py
+++ Ebay Analyzer/src/item.py
@@ -50,16 +50,49 @@ class Item:
                 print("Could not parse price for " + self.title)
                 break
             i += 1
-            
+        
+        try:
+            txt = elt.find_element_by_xpath(constants.TIMELEFT).text
+            print(txt)
+            self.time_left = parse_time(txt)
+        except:
+            self.time_left = None
+        
+        try:
+            txt = elt.find_element_by_xpath(constants.NUMBIDS).text
+            print(txt)
+            self.num_bids = int(txt.split("bid")[0].strip())
+        except:
+            self.num_bids = None
+        
         # add to mysql db
         cursor = db_conn.cursor()
-        add_item  = "INSERT INTO Items (EbayID,Title,SellerName,BidPrice,BINPrice) VALUES (%s,%s,%s,%s,%s)" \
-                    " ON DUPLICATE KEY UPDATE EbayID=EbayID,Title=Title,SellerName=SellerName,BidPrice=BidPrice,BINPrice=BINPrice;"
-        item_data = (self.ebayID, self.title, self.seller_name, self.AUCprice, self.BINprice)
-        cursor.execute(add_item, item_data)
-        db_conn.commit()
-        cursor.close()
-        
+        try:
+            add_item  = "INSERT INTO Items (EbayID,Title,SellerName,BidPrice,BINPrice,TimeLeft,NumBids) VALUES (%s,%s,%s,%s,%s,%s,%s)" \
+                        " ON DUPLICATE KEY UPDATE EbayID=EbayID,Title=Title,SellerName=SellerName,BidPrice=BidPrice,BINPrice=BINPrice,TimeLeft=TimeLeft,NumBids=NumBids;"
+            item_data = (self.ebayID, self.title, self.seller_name, self.AUCprice, self.BINprice, self.time_left, self.num_bids)
+            cursor.execute(add_item, item_data)
+            db_conn.commit()
+            cursor.close()
+        except:
+            print("Error uploading item w/ ID = %s to MySQL" % self.ebayID)
+        finally:
+            cursor.close()
     
     def toString(self):
         return self.title + ": " + str(self.BINprice) + "/" + str(self.AUCprice)
+
+"turns time_string into an integer number of seconds"
+def parse_time(time_string):
+    formatted_time = time_string.split("left")[0].strip()
+    seconds = re.match("(\d+)s", formatted_time)
+    minutes = re.match("(\d+)m", formatted_time)
+    hours = re.match("(\d+)h", formatted_time)
+    days = re.match("(\d+)d", formatted_time)
+    
+    seconds_left = 0
+    seconds_left += int(seconds.group(1)) if seconds else 0
+    seconds_left += 60*int(minutes.group(1)) if minutes else 0
+    seconds_left += 3600*int(hours.group(1)) if hours else 0
+    seconds_left += 24*3600*int(days.group(1)) if days else 0
+    return seconds_left # seconds
